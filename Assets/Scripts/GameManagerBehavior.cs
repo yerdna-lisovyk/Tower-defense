74f9dc146b0b921066c84f5be9df1c2ed0a26aba
Andrey <andrejdymovskih@gmail.com>
2022-07-21 14:33:52 +0300

    add Wave

--- Assets/Scripts/GameManagerBehavior.cs
+++ Assets/Scripts/GameManagerBehavior.cs
@@ -6,6 +6,31 @@ public class GameManagerBehavior : MonoBehaviour
 {
     public Text goldLabel;
     private int _gold;
+    
+    public Text waveLabel;
+    public GameObject[] nextWaveLabels;
+    public bool gameOver = false;
+    private int wave;
+    public int Wave
+    {
+        get
+        {
+            return wave;
+        }
+        set
+        {
+            wave = value;
+            if (!gameOver)
+            {
+                foreach (var t in nextWaveLabels)
+                {
+                    t.GetComponent<Animator>().SetTrigger("nextWave");
+                }
+            }
+            waveLabel.text = "WAVE: " + (wave + 1);
+        }
+    }
+    
     public int Gold {
         get => _gold;
         set
@@ -17,6 +42,7 @@ public class GameManagerBehavior : MonoBehaviour
 
     private void Start()
     {
+        Wave = 0;
         Gold = 1000;
     }
 }
