5080645e9423cf1f109f190b26a2ff860d8355e7
Andrey <andrejdymovskih@gmail.com>
2022-07-23 16:45:02 +0300

    add Health Bar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public float maxHealth = 100;
+    public float currentHealth = 100;
+    private float originalScale;
+
+    private void Start()
+    {
+        originalScale = gameObject.transform.localScale.x;
+    }
+
+    private void Update()
+    {
+        Vector3 tmpScale = gameObject.transform.localScale;
+        tmpScale.x = currentHealth / maxHealth * originalScale;
+        gameObject.transform.localScale = tmpScale;
+    }
+}
